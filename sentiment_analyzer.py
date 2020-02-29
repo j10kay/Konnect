8dbb9e1fd2debb70418fec5cf60566b380e1f206
Aimie Ojuba <41306649+Cytla24@users.noreply.github.com>
2020-02-28 19:42:32 -0500

    Update sentiment_analyzer.py

--- sentiment_analyzer.py
+++ sentiment_analyzer.py
@@ -8,3 +8,10 @@ class TweetAnalyzer():
 		return ' '.join(re.sub("(@[A-Za-z0-9]+)|([^0-9A-Za-z \t])|(\w+:\/\/\S+)", " ", intweet).split())
 
 	def sentimentAnalyzer(self, tweet):
+		trating = (analysis.sentiment.polarity + 1) * 5/2
+
+		srating = 5 - analysis.sentiment.subjectivity
+
+		frating = trating + srating
+
+		return frating
