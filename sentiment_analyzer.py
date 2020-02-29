4735b0d30eb279545be115ec2a694976e42818cd
Tamaraupreye <32442656+Tamaraupreye@users.noreply.github.com>
2020-02-28 21:28:46 -0500

    Update sentiment_analyzer.py
    Made some changes to the indentation. Added an __init__ function

--- sentiment_analyzer.py
+++ sentiment_analyzer.py
@@ -1,65 +1,42 @@
 from vaderSentiment.vaderSentiment import SentimentIntensityAnalyzer
-
 import csv
-
 import re
 
-class TweetAnalyzer():
 
-	# Function to clean tweets and remove 
-	def cleanTweet(self, intweet):
-		return ' '.join(re.sub("(@[A-Za-z0-9]+)|([^0-9A-Za-z \t])|(\w+:\/\/\S+)", " ", intweet).split())
+class TweetAnalyzer(): # Function to clean tweets and remove
+    def __init__(self):
+        pass
 
-	# Analyzes result from csv file
-	def sentimentAnalyzer(self, csvFileName = "myTweets.csv"):
-		# returns 
-		analysis = SentimentIntensityAnalyzer()
+    def cleanTweet(self, intweet):
+        return ' '.join(re.sub("(@[A-Za-z0-9]+)|([^0-9A-Za-z \t])|(\w+:\/\/\S+)", " ", intweet).split())
 
-		cur_tweets = self.ListOfTweets(csvFileName)
+    def sentimentAnalyzer(self, csvFileName = "myTweets.csv"): # Analyzes result from csv file
+        analysis = SentimentIntensityAnalyzer()
+        cur_tweets = self.listOfTweets(csvFileName)
+        frating = 0
+        count = 0
 
-		# commented out as we do not need to clean tweets
-		# cur_tweets = []
+        for tweet in cur_tweets:
+            vs = analysis.polarity_scores(tweet)
+            crating = (vs['compound'] + 1) * (5/2)
+            frating += crating
+            count += 1
 
-		# for tweet in uncleaned_cur_tweets:
-		# 	cur_tweets.append(self.cleanTweet(tweet))
+        return frating/count
 
-		frating = 0
-		count = 0
+    def listOfTweets(self, csvFileName):
+        with open(csvFileName) as csvfile:
+            readCSV = csv.reader(csvfile, delimiter = ",")
+            cur_tweets = []
 
-		for tweet in cur_tweets:
-			vs = analysis.polarity_scores(tweet)
+            for row in readCSV:
+                init_tweet = row[2]
+                cur_tweets.append(init_tweet)
 
-			crating = (vs['compound'] + 1) * 5/2
-			frating += crating
-			count += 1
+            i = 0
+            while i < len(cur_tweets):
+                temp = cur_tweets[i]
+                cur_tweets[i] = temp[2:len(temp)-1]
+                i += 1
 
-
-		return frating/count
-
-	# returns a list 
-	def ListOfTweets(self, csvFileName):
-		with open(csvFileName) as csvfile:
-			readCSV = csv.reader(csvfile, delimiter = ",")
-
-			cur_tweets = []
-
-
-			for row in readCSV:
-				init_tweet = row[2]
-				cur_tweets.append(init_tweet)
-
-			i = 0
-			while i < len(cur_tweets):
-				temp = cur_tweets[i]
-				cur_tweets[i] = temp[2:len(temp)-1]
-				i+= 1
-
-			return cur_tweets
-
-analyze = TweetAnalyzer()
-
-
-def main():
-    return
-
-if __name__ == "__main__":main()
+        return cur_tweets
